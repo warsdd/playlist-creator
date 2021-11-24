f46bb6b6b3225afb8a72d32211cbee7228ab9325
Sara Ward <saro4cka.ward@yandex.com>
2021-11-24 23:07:32 +0000

    updated js functionality

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-const container = document.querySelector(".container");
-const songsContainer = container.querySelector(".songs-container");
-const addButton = container.querySelector(".input__btn_action_add");
-const resetButton = container.querySelector(".input__btn_action_reset");
-const noSongsElement = container.querySelector(".no-songs");
-
-function renderHasSongs() {
-  resetButton.removeAttribute("disabled");
-  resetButton.classList.remove("input__btn_disabled");
-  noSongsElement.classList.add("no-songs_hidden");
-}
-
-function renderNoSongs() {
-  resetButton.setAttribute("disabled", true);
-  resetButton.classList.add("input__btn_disabled");
-  noSongsElement.classList.remove("no-songs_hidden");
-}
+const songsContainer = document.querySelector(".songs-container");
+const addButton = document.querySelector(".input__btn_action_add");
+const artistInput = document.querySelector(".input__text_type_artist");
+const titleInput = document.querySelector(".input__text_type_title");
 
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
-  const songElement = songTemplate.querySelector('.song').cloneNode(true);
+  const songElement = songTemplate.cloneNode(true);
 
   songElement.querySelector(".song__artist").textContent = artistValue;
   songElement.querySelector(".song__title").textContent = titleValue;
-  
+
   songsContainer.append(songElement);
+  artistInput.value = "";
+  titleInput.value = "";
+}
+
+function keyHandler(evt) {
+  if (evt.key === "Enter") {
+    addSong(artistInput.value, titleInput.value);
+  }
 }
 
 addButton.addEventListener("click", function () {
-  const artist = document.querySelector(".input__text_type_artist");
-  const title = document.querySelector(".input__text_type_title");
-
-  addSong(artist.value, title.value);
-  renderHasSongs();
-
-  artist.value = "";
-  title.value = "";
+  addSong(artistInput.value, titleInput.value);
 });
 
-resetButton.addEventListener("click", function () {
-  const songs = document.querySelectorAll(".song")
+artistInput.addEventListener("keydown", keyHandler);
+titleInput.addEventListener("keydown", keyHandler);
 
-  songs.forEach((item) => {
-    item.remove();
-  });
-
-  renderNoSongs();
+songsContainer.addEventListener("click", function (evt) {
+ if (evt.target.classList.contains("song__like")) {
+   evt.target.classList.toggle("song__like_active");
+ }
 });
